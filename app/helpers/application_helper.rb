93701f1312d218a5861e70c5d1b28e8e85a870ee
Reed Law <reed.law@rolemodelsoftware.com>
2023-07-19 16:51:39 -0400

    add active to button and remove buttons from sidebar (#1)
    * add active to button and remove buttons from sidebar
    
    * bundle
    
    * better previews
    
    * switch default
    
    * implement suggestions
    
    * rubocop
    
    * add button with icon preview
    
    * add url
    
    * bump version

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,7 @@
 # frozen_string_literal: true
 
 module ApplicationHelper
+  def component(name, *args, **kwargs, &)
+    render(name.camelize.constantize::Component.new(*args, **kwargs), &)
+  end
 end
